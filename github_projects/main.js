0f082b9233ce98d3670096cb05044e2f8a118414
Hakeem <hakeem.trotmanp1@gmail.com>
2020-08-20 13:07:20 -0400

    Final Project folder

---
+++ github_projects/main.js
@@ -0,0 +1,5 @@
+document.getElementById("grid-container").onclick = function() {myFunction()};
+
+function clickedGrid(){
+    document.getElementsByClassName("grid-item").innerHTML = ("Clicked");
+}
